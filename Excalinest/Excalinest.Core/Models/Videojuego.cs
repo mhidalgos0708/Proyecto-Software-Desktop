f70a2a68e640dd80b799bfd2180557a4d0d24e0b
Reyner-Arias <Rey86581@gmail.com>
2023-02-24 10:52:33 -0600

    PS-17 Finishing Videojuego mongo model

--- Excalinest/Excalinest.Core/Models/Videojuego.cs
+++ Excalinest/Excalinest.Core/Models/Videojuego.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Drawing;
 
 namespace Excalinest.Core.Models;
 internal class Videojuego
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? ID
+    {
+        get; set;
+    }
+
+    [BsonElement("titulo")]
+    public string Titulo { get; set; } = null!;
+
+    public Image Portada { get; set; } = null!;
+
+    public string Sinopsis { get; set; } = null!;
+
+    public string Usuario { get; set; } = null!;
+
+    public FileStream JuegoZIP { get; set; } = null!;
+
+    public List<string> Etiquetas { get; set; } = null!;
+
+    public List<Image> RedesSociales { get; set; } = null!;
 }
