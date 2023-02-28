ef62fa38863cc96f0f702ef17e90c2e117869f09
Reyner-Arias <Rey86581@gmail.com>
2023-02-27 21:25:30 -0600

    PS-20 Incomplete 'Ejecutar videojuego' method

--- Excalinest/Excalinest/Views/VideogamesDetailPage.xaml.cs
+++ Excalinest/Excalinest/Views/VideogamesDetailPage.xaml.cs
@@ -3,15 +3,17 @@
 using Excalinest.Contracts.Services;
 using Excalinest.ViewModels;
 
-
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using System.Diagnostics;
-
+using System.Reflection;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Excalinest.Views;
 
+// Código para la ventana de detalles de cada videojuego
 public sealed partial class VideogamesDetailPage : Page
 {
     public VideogamesDetailViewModel ViewModel
@@ -19,13 +21,13 @@ public sealed partial class VideogamesDetailPage : Page
         get;
     }
 
-    private readonly string Videojuego;
+    private readonly string NombreVideojuego;
 
     public VideogamesDetailPage()
     {
         ViewModel = App.GetService<VideogamesDetailViewModel>();
         InitializeComponent();
-        Videojuego = "Wednesday";
+        NombreVideojuego = "Wednesday";
     }
 
     protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -48,8 +50,30 @@ public sealed partial class VideogamesDetailPage : Page
         }
     }
 
+    // Método para ejecutar el videojuego actual, busca en la ruta del mismo nombre el ejecutable correspondiente
     public void EjecutarVideojuego(object sender, RoutedEventArgs e)
     {
-        Process.Start("Excalinest\\Assets\\Videojuegos\\", Videojuego);
+        var NombreExtension = NombreVideojuego + ".EXE";
+        var RutaJuego = @"Excalinest\Assets\Videojuegos\" + NombreVideojuego;
+        try
+        {
+            var VideojuegoEjecutable = Directory.GetFiles("D:\\", NombreExtension, SearchOption.AllDirectories)
+                    .Select(fileName => Path.GetFileNameWithoutExtension(fileName))
+                    .AsEnumerable()
+                    .ToArray();
+
+            for (var i = 0; i < VideojuegoEjecutable.Length; i++)
+            {
+                if (Path.GetFileName(Path.GetFileNameWithoutExtension(VideojuegoEjecutable[i])) == NombreVideojuego)
+                {
+                    var VideojuegoActual = Process.Start(VideojuegoEjecutable[i]);
+                }
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
