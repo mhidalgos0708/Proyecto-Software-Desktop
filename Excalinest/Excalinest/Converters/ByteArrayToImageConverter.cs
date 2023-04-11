130777b206164e38e790061da94c73d497cdc72f
Marianna Hidalgo S <mhidalgos0708@gmail.com>
2023-04-11 00:50:54 -0600

    PS-45 #issue-done #comment Grid displays the cover images

---
+++ Excalinest/Excalinest/Converters/ByteArrayToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace Excalinest.Converters;
+public class ByteArrayToImageConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string culture)
+    {
+        if (value is not byte[] imageData) return null;
+
+        BitmapImage bitmapImage = new BitmapImage();
+        using (MemoryStream memoryStream = new MemoryStream(imageData))
+        {
+            bitmapImage.SetSource(memoryStream.AsRandomAccessStream());
+        }
+
+        return bitmapImage;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- Excalinest/Excalinest/Views/VideogamesPage.xaml.cs
+++ Excalinest/Excalinest/Views/VideogamesPage.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System.Diagnostics;
 using Excalinest.Core.Models;
 using Excalinest.ViewModels;
-
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media.Imaging;
-
 namespace Excalinest.Views;
 
 public sealed partial class VideogamesPage : Page
 {
-    public BitmapImage cover;
-    public StackPanel myStackPanel;
 
     public VideogamesViewModel ViewModel
     {
@@ -21,48 +16,14 @@ public sealed partial class VideogamesPage : Page
     {
         ViewModel = App.GetService<VideogamesViewModel>();
         InitializeComponent();
-
-        // taglist.ItemsSource = ViewModel.Tags;
     }
-
-
-
-    /*public async void GetPortadas()
-    {
-        List<BitmapImage> images = new List<BitmapImage>();
-        // Assuming you have a list of MongoDB documents named "documents" containing image data as a byte array
-        var imageControls = new List<Image>();
-
-        foreach (var document in ViewModel.Source)
-        {
-            using (var memoryStream = new MemoryStream(document.Portada.Data))
-            {
-                var bitmapImage = new BitmapImage();
-                await bitmapImage.SetSourceAsync(memoryStream.AsRandomAccessStream());
-
-                var imageControl = new Image();
-                imageControl.Source = bitmapImage;
-
-                imageControls.Add(imageControl);
-                images.Add(bitmapImage);
-            }
-        }
-
-        // Add the image controls to your UI as needed
-        foreach (var imageControl in imageControls)
-        {
-            myStackPanel.Children.Add(imageControl); // Assumes you have a StackPanel named "myStackPanel" in your UI
-            
-        }
-    }*/
 
     public async void TagComboBox_SelectionChanged(object sender, SelectionChangedEventArgs args)
     {
-        ComboBox tag = (ComboBox)sender;
-        Tag chosenTag = tag.SelectedItem as Tag;
-        Debug.WriteLine(chosenTag.ID);
-        await ViewModel.GetVideojuegosByTag(chosenTag.ID);
+        ComboBox tag = (ComboBox) sender;
+        if (tag.SelectedItem is Tag chosenTag)
+        {
+            await ViewModel.GetVideojuegosByTag(chosenTag.ID);
+        }
     }
-
-    
 }
